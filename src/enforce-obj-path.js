7027b96a28de7ef1f9363d6c994bae15213eec63
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-05-12 11:39:28 +0200

    feat: Enforce Object Path module
    Checks if a given path in an object is defined, and if not throws an
    exception with a well formed error message.

---
+++ src/enforce-obj-path.js
@@ -0,0 +1,15 @@
+import {has} from 'lodash/object'
+
+export default function enforceObjPath (obj, path) {
+  if (!has(obj, path)) {
+    const err = new Error()
+    err.name = 'PropertyMissing'
+    err.message = `Required property ${path} missing from:
+
+${JSON.stringify(obj)}
+
+`
+    throw err
+  }
+  return true
+}
