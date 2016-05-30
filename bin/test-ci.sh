55255e61ea1bd109caec82dd106ebdfb8bf70414
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-05-30 16:10:45 +0200

    feat: add new test-ci.sh script

---
+++ bin/test-ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Build script used by the SDKs to run all the necessary tasks for the CI build
+#
+
+# Unit tests, with coverage generation
+npm run test:cover
+
+# Create the CommonJS and browser builds, so we can run integration tests using those
+npm run build
+
+# Run the node integration tests
+npm run test:integration
+
+# We only want to run the browser tests on one of the multiple node versions we test on
+if ./node_modules/contentful-sdk-core/bin/run-if-node-version.js ; then
+  npm run test:browser-remote
+fi
