81f79f5c69399de900592605409709c110f8d8ae
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-06-02 11:13:39 +0200

    fix: Run integration tests on only one node version

--- bin/test-ci.sh
+++ bin/test-ci.sh
@@ -11,8 +11,10 @@ npm run test:cover
 # Create the CommonJS and browser builds, so we can run integration tests using those
 npm run build
 
-# Run the node integration tests
-npm run test:integration
+# Run the node integration tests. Running them on only one version should be enough.
+if ./node_modules/contentful-sdk-core/bin/run-if-node-version.js ; then
+  npm run test:integration
+fi
 
 # We only want to run the browser tests on one of the multiple node versions we test on
 if ./node_modules/contentful-sdk-core/bin/run-if-node-version.js ; then
