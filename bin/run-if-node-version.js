62bf411a022137454b18be192585e7644b64fc9e
Khaled Garbaya <khaled@contentful.com>
2017-02-28 15:17:36 +0100

    build(ci): fix linting

--- bin/run-if-node-version.js
+++ bin/run-if-node-version.js
@@ -2,13 +2,13 @@
 
 /**
  * This script exits unless the TRAVIS_NODE_VERSION environment variable
- * contains a 5.x version.
+ * contains a 6.x version.
  *
  * This is used in package.json to ensure that we are only firing the remote
  * saucelabs browser tests from one of the builds we are running on Travis
  */
 
-var re = new RegExp(process.env.LEAD_NODE_MAJOR_VERSION + '\.', 'g')
+var re = new RegExp(process.env.LEAD_NODE_MAJOR_VERSION + '.', 'g')
 
 if (!re.test(process.env.TRAVIS_NODE_VERSION)) {
   process.exit(1)
