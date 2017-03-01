d7d5b800dae7c1cad3167793e9dc3003d07406af
Khaled Garbaya <khaled@contentful.com>
2017-03-01 11:49:04 +0100

    fix(ci): Remove node v7 from travis config

--- bin/release-ci.sh
+++ bin/release-ci.sh
@@ -4,6 +4,6 @@ set -e
 #
 # run semantic release only in one version
 #
-if ./bin/run-if-node-version.js ; then
+if ./node_modules/contentful-sdk-core/bin/run-if-node-version.js ; then
   npm run semantic-release
 fi
