2386138977071eb23c88a1f5d60e2ac476ad79c1
Benedikt Rötsch <axe312ger@users.noreply.github.com>
2017-05-31 15:27:24 +0200

    feat(error-retry): add retryOnError config to disable error retry

--- test/unit/rate-limit-test.js
+++ test/unit/rate-limit-test.js
@@ -2,64 +2,78 @@ import test from 'blue-tape'
 import rateLimit from '../../lib/rate-limit'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-let mock
+
+const mock = new MockAdapter(axios)
+
 function setup () {
-  mock = new MockAdapter(axios)
-  rateLimit(axios)
+  const client = axios.create({ retryOnError: true })
+  rateLimit(client)
+  return { client }
+}
+
+function setupWithoutErrorRetry () {
+  const client = axios.create({ retryOnError: false })
+  rateLimit(client)
+  return { client }
+}
+function setupWithOneRetry () {
+  const client = axios.create({ retryOnError: true })
+  rateLimit(client, 2)
+  return { client }
 }
 function teardown () {
   mock.reset()
 }
-test('Retry on 429', (t) => {
-  setup()
-  mock.onGet('/rate-limit-me').replyOnce(429, 'rateLimited', {'x-contentful-ratelimit-reset': 2})
-  mock.onGet('/rate-limit-me').replyOnce(200, 'works')
-  t.plan(2)
-  return axios.get('/rate-limit-me').then((response) => {
-    t.ok(response.data)
-    t.equals(response.data, 'works')
-    teardown()
-  })
-})
-test('Retry after a duration >= rateLimit header', (t) => {
-  setup()
-  mock.onGet('/rate-limit-me').replyOnce(429, 'rateLimited', {'x-contentful-ratelimit-reset': 2})
+test('Retry on 429 after a duration >= rateLimit header', (t) => {
+  const { client } = setup()
+  mock.onGet('/rate-limit-me').replyOnce(429, 'rateLimited', {'x-contentful-ratelimit-reset': 4})
   mock.onGet('/rate-limit-me').replyOnce(200, 'works')
   const startTime = Date.now()
   t.plan(3)
-  return axios.get('/rate-limit-me').then((response) => {
+  return client.get('/rate-limit-me').then((response) => {
     t.ok(response.data)
     t.equals(response.data, 'works')
-    t.ok(Date.now() - startTime >= 2000)
+    t.ok(Date.now() - startTime >= 4000)
     teardown()
   })
 })
 test('Retry on 500', (t) => {
-  setup()
-  mock.onGet('/rate-limit-me').replyOnce(500)
-  mock.onGet('/rate-limit-me').replyOnce(500)
-  mock.onGet('/rate-limit-me').replyOnce(500)
+  const { client } = setup()
   mock.onGet('/rate-limit-me').replyOnce(500)
   mock.onGet('/rate-limit-me').replyOnce(500)
   mock.onGet('/rate-limit-me').replyOnce(200, 'works')
   t.plan(2)
-  return axios.get('/rate-limit-me').then((response) => {
+  return client.get('/rate-limit-me').then((response) => {
     t.ok(response.data)
     t.equals(response.data, 'works')
     teardown()
   })
 })
-test('Should Fail if it hits maxRetries', (t) => {
-  setup()
-  for (let i = 0; i < 5; i++) {
-    mock.onGet('/error').replyOnce(500, `error attempt ${i + 1}`)
-  }
-  t.plan(1)
-  return axios.get('/error').then((response) => {
-    t.fail('the request should return error')
-  }).catch((error) => {
-    t.ok(error)
+test('no retry when automatic handling flag is disabled', (t) => {
+  const { client } = setupWithoutErrorRetry()
+  const responseError = new Error('Mocked 500 Error')
+  mock.onGet('/rate-limit-me').replyOnce(500, responseError)
+  t.plan(2)
+  return client.get('/rate-limit-me').then((response) => {
+    t.fail('Promise should reject not resolve')
+    teardown()
+  })
+  .catch((error) => {
+    t.equals(error.response.status, 500)
+    t.equals(error.response.data, responseError)
     teardown()
   })
 })
-
+test('Should Fail if it hits maxRetries', (t) => {
+  const { client } = setupWithOneRetry()
+  mock.onGet('/error').replyOnce(500, 'error attempt #1')
+  mock.onGet('/error').replyOnce(500, 'error attempt #2')
+  t.plan(2)
+  return client.get('/error').then((response) => {
+    t.fail('the request should return error')
+  }).catch((error) => {
+    t.ok(error)
+    t.equals(error.response.data, 'error attempt #2')
+    teardown()
+  })
+})
