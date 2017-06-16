45a66dbb375e0d40f76f4079b20072f2d28d3861
Benedikt Rötsch <benedikt@contentful.com>
2017-06-16 14:24:47 +0200

    feat(logging): adds log handling for error and log events

--- test/unit/rate-limit-test.js
+++ test/unit/rate-limit-test.js
@@ -1,28 +1,41 @@
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import test from 'blue-tape'
+import sinon from 'sinon'
 
 import rateLimit from '../../lib/rate-limit'
+
+const logHandlerStub = sinon.stub()
 
 const mock = new MockAdapter(axios)
 
 function setup () {
-  const client = axios.create({ retryOnError: true })
+  const client = axios.create({
+    logHandler: logHandlerStub,
+    retryOnError: true
+  })
   rateLimit(client)
   return { client }
 }
 
 function setupWithoutErrorRetry () {
-  const client = axios.create({ retryOnError: false })
+  const client = axios.create({
+    logHandler: logHandlerStub,
+    retryOnError: false
+  })
   rateLimit(client)
   return { client }
 }
 function setupWithOneRetry () {
-  const client = axios.create({ retryOnError: true })
+  const client = axios.create({
+    logHandler: logHandlerStub,
+    retryOnError: true
+  })
   rateLimit(client, 2)
   return { client }
 }
 function teardown () {
+  logHandlerStub.reset()
   mock.reset()
 }
 test('Retry on 429 after a duration >= rateLimit header', (t) => {
@@ -30,10 +43,13 @@ test('Retry on 429 after a duration >= rateLimit header', (t) => {
   mock.onGet('/rate-limit-me').replyOnce(429, 'rateLimited', {'x-contentful-ratelimit-reset': 4})
   mock.onGet('/rate-limit-me').replyOnce(200, 'works')
   const startTime = Date.now()
-  t.plan(3)
+  t.plan(6)
   return client.get('/rate-limit-me').then((response) => {
     t.ok(response.data)
     t.equals(response.data, 'works')
+    t.equals(logHandlerStub.callCount, 1)
+    t.equals(logHandlerStub.args[0][0], 'warning')
+    t.ok(logHandlerStub.args[0][1].includes, 'Rate limit error occured.')
     t.ok(Date.now() - startTime >= 4000)
     teardown()
   })
@@ -73,10 +89,9 @@ test.skip('Retry on network error', (t) => {
 })
 test('no retry when automatic handling flag is disabled', (t) => {
   const { client } = setupWithoutErrorRetry()
-  const responseError = new Error('Mocked 500 Error')
-  mock.onGet('/rate-limit-me').replyOnce(500, responseError, {'x-contentful-request-id': 3})
+  mock.onGet('/rate-limit-me').replyOnce(500, 'Mocked 500 Error', {'x-contentful-request-id': 3})
   mock.onGet('/rate-limit-me').replyOnce(200, 'would work but retry is disabled', {'x-contentful-request-id': 4})
-  t.plan(3)
+
   return client.get('/rate-limit-me').then((response) => {
     t.fail('Promise should reject not resolve')
     teardown()
@@ -84,21 +99,31 @@ test('no retry when automatic handling flag is disabled', (t) => {
   .catch((error) => {
     t.equals(error.response.status, 500)
     t.equals(error.response.headers['x-contentful-request-id'], 3)
-    t.equals(error.response.data, responseError)
+    t.equals(error.response.data, 'Mocked 500 Error')
+    t.equals(logHandlerStub.callCount, 1)
+    t.equals(logHandlerStub.args[0][0], 'error')
+    t.equals(logHandlerStub.args[0][1].message, 'Request failed with status code 500')
+    t.equals(logHandlerStub.args[0][1].attempts, undefined)
     teardown()
   })
 })
 test('Should Fail if it hits maxRetries', (t) => {
   const { client } = setupWithOneRetry()
   mock.onGet('/error').replyOnce(500, 'error attempt #1', {'x-contentful-request-id': 4})
-  mock.onGet('/error').replyOnce(500, 'error attempt #2', {'x-contentful-request-id': 4})
-  t.plan(2)
+  mock.onGet('/error').replyOnce(501, 'error attempt #2', {'x-contentful-request-id': 4})
+  mock.onGet('/error').replyOnce(200, 'should not be there', {'x-contentful-request-id': 4})
+
   return client.get('/error').then((response) => {
     t.fail('the request should return error')
     teardown()
   }).catch((error) => {
     t.ok(error)
-    t.equals(error.response.data, 'error attempt #2')
+    t.equals(error.response.data, 'error attempt #2', 'returned error since maxRetries was reached')
+    t.equals(logHandlerStub.callCount, 2)
+    t.equals(logHandlerStub.args[0][0], 'warning', 'first log is a warning that it retries')
+    t.equals(logHandlerStub.args[1][0], 'error', 'second log is an error that it failed')
+    t.equals(logHandlerStub.args[1][1].message, 'Request failed with status code 501')
+    t.equals(logHandlerStub.args[1][1].attempts, 2, 'logs the correct amounth of attempts')
     teardown()
   })
 })
@@ -106,27 +131,33 @@ test('Rejects error straight away when X-Contentful-Request-Id header is missing
   const { client } = setupWithOneRetry()
   mock.onGet('/error').replyOnce(500, 'error attempt')
   mock.onGet('/error').replyOnce(200, 'works')
-  t.plan(2)
+
   return client.get('/error').then((response) => {
     t.fail('the request should return error')
     teardown()
   }).catch((error) => {
     t.ok(error)
     t.equals(error.response.data, 'error attempt')
+    t.equals(logHandlerStub.callCount, 1)
+    t.equals(logHandlerStub.args[0][0], 'error')
+    t.equals(logHandlerStub.args[0][1].message, 'Request failed with status code 500')
+    t.equals(logHandlerStub.args[0][1].attempts, 1)
     teardown()
   })
 })
 test('Rejects errors with strange status codes', (t) => {
-  const { client } = setupWithOneRetry()
+  const { client } = setup()
   mock.onGet('/error').replyOnce(765, 'error attempt')
   mock.onGet('/error').replyOnce(200, 'works')
-  t.plan(2)
+
   return client.get('/error').then((response) => {
     t.fail('the request should return error')
     teardown()
   }).catch((error) => {
     t.ok(error)
     t.equals(error.response.data, 'error attempt')
+    t.equals(error.response.data, 'error attempt')
+    t.equals(logHandlerStub.callCount, 1)
     teardown()
   })
 })
