7a7185588b0444a120a91ef9616ac29c77627acf
Benedikt Rötsch <benedikt@contentful.com>
2017-06-16 14:24:47 +0200

    feat(logging): adds log handling for error and log events

--- test/unit/create-http-client-test.js
+++ test/unit/create-http-client-test.js
@@ -6,6 +6,7 @@ import createHttpClient, {__RewireAPI__ as createHttpClientRewireApi} from '../.
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 
+const logHandlerStub = sinon.stub()
 const mock = new MockAdapter(axios)
 function setup () {
   createHttpClientRewireApi.__Rewire__('rateLimit', sinon.stub())
@@ -22,10 +23,12 @@ test('Calls axios with expected default URL', t => {
   createHttpClient(axios, {
     accessToken: 'clientAccessToken',
     space: 'clientSpaceId',
-    defaultHostname: 'defaulthost'
+    defaultHostname: 'defaulthost',
+    logHandler: logHandlerStub
   })
 
   t.equals(axios.create.args[0][0].baseURL, 'https://defaulthost:443/spaces/clientSpaceId/')
+  t.equals(logHandlerStub.callCount, 0, 'does not log anything')
   teardown()
   t.end()
 })
@@ -34,10 +37,12 @@ test('Calls axios based on passed host', t => {
   setup()
   createHttpClient(axios, {
     accessToken: 'clientAccessToken',
-    host: 'contentful.com:8080'
+    host: 'contentful.com:8080',
+    logHandler: logHandlerStub
   })
 
   t.equals(axios.create.args[0][0].baseURL, 'https://contentful.com:8080/spaces/')
+  t.equals(logHandlerStub.callCount, 0, 'does not log anything')
   teardown()
   t.end()
 })
@@ -47,10 +52,12 @@ test('Calls axios based on passed host with insecure flag', t => {
   createHttpClient(axios, {
     accessToken: 'clientAccessToken',
     host: 'contentful.com:321',
-    insecure: true
+    insecure: true,
+    logHandler: logHandlerStub
   })
 
   t.equals(axios.create.args[0][0].baseURL, 'http://contentful.com:321/spaces/')
+  t.equals(logHandlerStub.callCount, 0, 'does not log anything')
   teardown()
   t.end()
 })
@@ -60,10 +67,33 @@ test('Calls axios based on passed hostname with insecure flag', t => {
   createHttpClient(axios, {
     accessToken: 'clientAccessToken',
     host: 'contentful.com',
-    insecure: true
+    insecure: true,
+    logHandler: logHandlerStub
   })
 
   t.equals(axios.create.args[0][0].baseURL, 'http://contentful.com:80/spaces/')
+  t.equals(logHandlerStub.callCount, 0, 'does not log anything')
   teardown()
   t.end()
+})
+
+test('Fails with missing access token', t => {
+  setup()
+  try {
+    createHttpClient(axios, {
+      logHandler: logHandlerStub
+    })
+    t.fail('should fail')
+    teardown()
+    t.end()
+  } catch (err) {
+    t.ok(err instanceof TypeError, 'throws a TypeError')
+    t.equals(err.message, 'Expected parameter accessToken')
+    t.equals(logHandlerStub.callCount, 1, 'logs only once')
+    t.equals(logHandlerStub.args[0][0], 'error', 'logs an error')
+    t.equals(logHandlerStub.args[0][1].message, 'Expected parameter accessToken', 'logged error has correct message')
+    t.ok(logHandlerStub.args[0][1] instanceof TypeError, 'logs a TypeError')
+    teardown()
+    t.end()
+  }
 })
