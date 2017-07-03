e5d4f0be3c0dd53dba6f88bded83015ad1ceb578
Benedikt Rötsch <benedikt@contentful.com>
2017-07-03 11:54:27 +0200

    build(npm): upgrade eslint and fix lint errors

--- test/unit/rate-limit-test.js
+++ test/unit/rate-limit-test.js
@@ -80,14 +80,14 @@ test('Retry on network error', (t) => {
   mock.onGet('/rate-limit-me').networkError()
 
   return client.get('/rate-limit-me')
-  .then((response) => {
-    t.fail('should not succeed')
-    teardown()
-  })
-  .catch((error) => {
-    t.equals(error.attempts, 2, 'logs two attempts, one initial and one retry')
-    teardown()
-  })
+    .then((response) => {
+      t.fail('should not succeed')
+      teardown()
+    })
+    .catch((error) => {
+      t.equals(error.attempts, 2, 'logs two attempts, one initial and one retry')
+      teardown()
+    })
 })
 
 test('no retry when automatic handling flag is disabled', (t) => {
@@ -95,20 +95,21 @@ test('no retry when automatic handling flag is disabled', (t) => {
   mock.onGet('/rate-limit-me').replyOnce(500, 'Mocked 500 Error', {'x-contentful-request-id': 3})
   mock.onGet('/rate-limit-me').replyOnce(200, 'would work but retry is disabled', {'x-contentful-request-id': 4})
 
-  return client.get('/rate-limit-me').then((response) => {
-    t.fail('Promise should reject not resolve')
-    teardown()
-  })
-  .catch((error) => {
-    t.equals(error.response.status, 500)
-    t.equals(error.response.headers['x-contentful-request-id'], 3)
-    t.equals(error.response.data, 'Mocked 500 Error')
-    t.equals(logHandlerStub.callCount, 1)
-    t.equals(logHandlerStub.args[0][0], 'error')
-    t.equals(logHandlerStub.args[0][1].message, 'Request failed with status code 500')
-    t.equals(logHandlerStub.args[0][1].attempts, undefined)
-    teardown()
-  })
+  return client.get('/rate-limit-me')
+    .then((response) => {
+      t.fail('Promise should reject not resolve')
+      teardown()
+    })
+    .catch((error) => {
+      t.equals(error.response.status, 500)
+      t.equals(error.response.headers['x-contentful-request-id'], 3)
+      t.equals(error.response.data, 'Mocked 500 Error')
+      t.equals(logHandlerStub.callCount, 1)
+      t.equals(logHandlerStub.args[0][0], 'error')
+      t.equals(logHandlerStub.args[0][1].message, 'Request failed with status code 500')
+      t.equals(logHandlerStub.args[0][1].attempts, undefined)
+      teardown()
+    })
 })
 test('Should Fail if it hits maxRetries', (t) => {
   const { client } = setupWithOneRetry()
@@ -116,51 +117,55 @@ test('Should Fail if it hits maxRetries', (t) => {
   mock.onGet('/error').replyOnce(501, 'error attempt #2', {'x-contentful-request-id': 4})
   mock.onGet('/error').replyOnce(200, 'should not be there', {'x-contentful-request-id': 4})
 
-  return client.get('/error').then((response) => {
-    t.fail('the request should return error')
-    teardown()
-  }).catch((error) => {
-    t.ok(error)
-    t.equals(error.response.data, 'error attempt #2', 'returned error since maxRetries was reached')
-    t.equals(logHandlerStub.callCount, 2)
-    t.equals(logHandlerStub.args[0][0], 'warning', 'first log is a warning that it retries')
-    t.equals(logHandlerStub.args[1][0], 'error', 'second log is an error that it failed')
-    t.equals(logHandlerStub.args[1][1].message, 'Request failed with status code 501')
-    t.equals(logHandlerStub.args[1][1].attempts, 2, 'logs the correct amounth of attempts')
-    teardown()
-  })
+  return client.get('/error')
+    .then((response) => {
+      t.fail('the request should return error')
+      teardown()
+    })
+    .catch((error) => {
+      t.ok(error)
+      t.equals(error.response.data, 'error attempt #2', 'returned error since maxRetries was reached')
+      t.equals(logHandlerStub.callCount, 2)
+      t.equals(logHandlerStub.args[0][0], 'warning', 'first log is a warning that it retries')
+      t.equals(logHandlerStub.args[1][0], 'error', 'second log is an error that it failed')
+      t.equals(logHandlerStub.args[1][1].message, 'Request failed with status code 501')
+      t.equals(logHandlerStub.args[1][1].attempts, 2, 'logs the correct amounth of attempts')
+      teardown()
+    })
 })
 test('Rejects error straight away when X-Contentful-Request-Id header is missing', (t) => {
   const { client } = setupWithOneRetry()
   mock.onGet('/error').replyOnce(500, 'error attempt')
   mock.onGet('/error').replyOnce(200, 'works')
 
-  return client.get('/error').then((response) => {
-    t.fail('the request should return error')
-    teardown()
-  }).catch((error) => {
-    t.ok(error)
-    t.equals(error.response.data, 'error attempt')
-    t.equals(logHandlerStub.callCount, 1)
-    t.equals(logHandlerStub.args[0][0], 'error')
-    t.equals(logHandlerStub.args[0][1].message, 'Request failed with status code 500')
-    t.equals(logHandlerStub.args[0][1].attempts, 1)
-    teardown()
-  })
+  return client.get('/error')
+    .then((response) => {
+      t.fail('the request should return error')
+      teardown()
+    }).catch((error) => {
+      t.ok(error)
+      t.equals(error.response.data, 'error attempt')
+      t.equals(logHandlerStub.callCount, 1)
+      t.equals(logHandlerStub.args[0][0], 'error')
+      t.equals(logHandlerStub.args[0][1].message, 'Request failed with status code 500')
+      t.equals(logHandlerStub.args[0][1].attempts, 1)
+      teardown()
+    })
 })
 test('Rejects errors with strange status codes', (t) => {
   const { client } = setup()
   mock.onGet('/error').replyOnce(765, 'error attempt')
   mock.onGet('/error').replyOnce(200, 'works')
 
-  return client.get('/error').then((response) => {
-    t.fail('the request should return error')
-    teardown()
-  }).catch((error) => {
-    t.ok(error)
-    t.equals(error.response.data, 'error attempt')
-    t.equals(error.response.data, 'error attempt')
-    t.equals(logHandlerStub.callCount, 1)
-    teardown()
-  })
+  return client.get('/error')
+    .then((response) => {
+      t.fail('the request should return error')
+      teardown()
+    }).catch((error) => {
+      t.ok(error)
+      t.equals(error.response.data, 'error attempt')
+      t.equals(error.response.data, 'error attempt')
+      t.equals(logHandlerStub.callCount, 1)
+      teardown()
+    })
 })
