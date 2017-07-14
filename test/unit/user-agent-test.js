d67f1ec0b6b787ba9849b020fa6cfd708dc0c98b
Benedikt Rötsch <benedikt@contentful.com>
2017-07-14 13:37:38 +0200

    test(user-agent): allow node versions with double digits like v6.11.1

--- test/unit/user-agent-test.js
+++ test/unit/user-agent-test.js
@@ -3,7 +3,7 @@ import getUserAgent from '../../lib/get-user-agent'
 
 test('Parse User agent correctly', (t) => {
   t.plan(1)
-  const headerRegEx = /(app|sdk|platform|integration|os) (\S+\/\d.\d.\d)(-\w+)?/igm
+  const headerRegEx = /(app|sdk|platform|integration|os) (\S+\/\d+.\d+.\d+)(-\w+)?/igm
   const userAgent = getUserAgent('contentful.js/1.0.0', 'myApplication/1.0.0', 'myIntegration/1.0.0')
   t.equal(userAgent.match(headerRegEx).length, 5)
 })
