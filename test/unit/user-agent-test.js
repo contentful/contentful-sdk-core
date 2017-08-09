7349547fa04d6ac4e0d88cb2ba5902db25931bc9
Benedikt Rötsch <benedikt@contentful.com>
2017-08-09 14:08:10 +0200

    fix(react-native): replace platform.js with custom code to reduce bundle size and make the lib work with react native again

--- test/unit/user-agent-test.js
+++ test/unit/user-agent-test.js
@@ -1,9 +1,51 @@
 import test from 'blue-tape'
-import getUserAgent from '../../lib/get-user-agent'
+import getUserAgent, {__RewireAPI__ as getUserAgentRewireApi} from '../../lib/get-user-agent'
 
-test('Parse User agent correctly', (t) => {
-  t.plan(1)
-  const headerRegEx = /(app|sdk|platform|integration|os) (\S+\/\d+.\d+.\d+)(-\w+)?/igm
+const headerRegEx = /(app|sdk|platform|integration|os) \S+(\/\d+.\d+.\d+(-[\w\d-]+)?)?;/igm
+
+test('Parse node user agent correctly', (t) => {
   const userAgent = getUserAgent('contentful.js/1.0.0', 'myApplication/1.0.0', 'myIntegration/1.0.0')
-  t.equal(userAgent.match(headerRegEx).length, 5)
+  t.equal(userAgent.match(headerRegEx).length, 5, 'consists of 5 parts')
+  t.true(userAgent.indexOf('platform node.js/') !== -1, 'detects node.js platform')
+  t.end()
+})
+
+test('Parse browser user agent correctly', (t) => {
+  // Fake browser environment
+  getUserAgentRewireApi.__Rewire__('isNode', () => false)
+  getUserAgentRewireApi.__Rewire__('isReactNative', () => false)
+  getUserAgentRewireApi.__Rewire__('window', {
+    navigator: {
+      platform: 'MacIntel'
+    }
+  })
+
+  const userAgent = getUserAgent('contentful.js/1.0.0', 'myApplication/1.0.0', 'myIntegration/1.0.0')
+  t.equal(userAgent.match(headerRegEx).length, 5, 'consists of 5 parts')
+  t.true(userAgent.indexOf('os macOS;') !== -1, 'detects correct os')
+  t.true(userAgent.indexOf('platform browser;') !== -1, 'detects browser platform')
+  t.end()
+  getUserAgentRewireApi.__ResetDependency__('isNode')
+  getUserAgentRewireApi.__ResetDependency__('isReactNative')
+  getUserAgentRewireApi.__ResetDependency__('window')
+})
+
+test('Parse react native user agent correctly', (t) => {
+  // Fake react native environment
+  getUserAgentRewireApi.__Rewire__('isNode', () => false)
+  getUserAgentRewireApi.__Rewire__('isReactNative', () => true)
+  getUserAgentRewireApi.__Rewire__('window', {
+    navigator: {
+      product: 'ReactNative'
+    }
+  })
+
+  const userAgent = getUserAgent('contentful.js/1.0.0', 'myApplication/1.0.0', 'myIntegration/1.0.0')
+  t.equal(userAgent.match(headerRegEx).length, 4, 'consists of 4 parts since os is missing in mocked data')
+  t.true(userAgent.indexOf('platform ReactNative') !== -1, 'detects react native platform')
+  t.end()
+
+  getUserAgentRewireApi.__ResetDependency__('isNode')
+  getUserAgentRewireApi.__ResetDependency__('isReactNative')
+  getUserAgentRewireApi.__ResetDependency__('window')
 })
