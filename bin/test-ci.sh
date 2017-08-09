0ab1e05a5e99d6c4b6e01500fd1e2eff1902913d
Benedikt Rötsch <benedikt@contentful.com>
2017-08-09 11:41:17 +0200

    fix(tests): revert to old test structure
    This reverts commit 6518ce03f4078d8a732b6dd7a701860568188152.

--- bin/test-ci.sh
+++ bin/test-ci.sh
@@ -9,7 +9,7 @@ set -e
 if ./node_modules/contentful-sdk-core/bin/run-if-node-version.js ; then
   npm run test:cover
 else
-  npm run test:unit
+  npm run test:only
 fi
 
 # Create the CommonJS and browser builds, so we can run integration tests using those
