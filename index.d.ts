48e22f04c8657ad49a1c19b4de0aec1324fee3cb
Khaled Garbaya <khaled@contentful.com>
2018-06-04 11:29:58 +0200

    fixup! fix: Add index.d.ts to files

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 import { AxiosInstance, AxiosStatic } from '@contentful/axios';
 
-
 export function createHttpClient(
   axios: AxiosInstance,
   options: { [key: string]: any }
@@ -8,6 +7,22 @@ export function createHttpClient(
 
 export function getUserAgentHeader(
   sdk: string,
-  application: string,
-  integration: string
+  application?: string,
+  integration?: string
 ): string;
+
+export interface ContentfulQuery {
+  content_type?: string;
+  'sys.id'?: string;
+  [key: string]: any;
+}
+
+interface ContentfulRequestConfig {
+  // TODO: omit `resovleLinks`
+  params: ContentfulQuery;
+}
+export function createRequestConfig({
+  query
+}: {
+  query: ContentfulQuery;
+}): ContentfulRequestConfig;
