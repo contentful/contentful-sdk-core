19076204c06e658c399b1a4fb2c9040b920ac8b5
Alexander Suevalov <alex.suevalov@contentful.com>
2020-12-01 14:09:13 +0100

    chore: enable gradual typescript migration (#111)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
-  'extends': 'standard',
-  'plugins': [
-    'standard',
-    'promise'
+  overrides: [
+    {
+      files: '**/*.js',
+      parser: 'babel-eslint',
+      extends: ['standard'],
+      plugins: ['standard', 'promise']
+    },
+    {
+      files: '**/*.ts',
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended'
+      ],
+      plugins: ['promise']
+    }
   ]
 }
