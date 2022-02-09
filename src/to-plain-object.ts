061238a182b7e8ad8a6555938fd7a569589ea33f
Marco Link <marco.link@contentful.com>
2022-02-09 17:09:56 +0100

    fix(types): toPlainObject return type

--- src/to-plain-object.ts
+++ src/to-plain-object.ts
@@ -6,7 +6,16 @@ import copy from 'fast-copy'
  * @param data - Any plain JSON response returned from the API
  * @return Enhanced object with toPlainObject method
  */
-export default function toPlainObject(data: unknown) {
+export default function toPlainObject<T = Record<string, unknown>, R = T>(
+  data: T
+): T & {
+  /**
+   * Returns this entity as a plain JS object
+   */
+  toPlainObject(): R
+} {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
   return Object.defineProperty(data, 'toPlainObject', {
     enumerable: false,
     configurable: false,
