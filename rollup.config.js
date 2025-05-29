81777706ba70ac68ac1c2d03c5173155eb39b915
Benedikt Rötsch <benedikt@hashbite.net>
2025-05-29 18:38:20 +0200

    refactor: turn into hybrid package

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+const esmConfig = {
+  input: 'dist/esm-raw/index.js',
+  output: {
+    dir: 'dist/esm',
+    format: 'esm',
+    preserveModules: true,
+  }
+}
+
+const cjsConfig = {
+  input: 'dist/esm-raw/index.js',
+  output: {
+    dir: 'dist/cjs',
+    format: 'cjs',
+    preserveModules: true,
+    entryFileNames: '[name].cjs',
+  }
+}
+
+export default [esmConfig, cjsConfig]
