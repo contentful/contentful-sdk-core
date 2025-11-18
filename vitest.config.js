6dc95d382167c1d39ecdc862fa14b7100a7d07a5
Ethan Ozelius <ethan.ozelius@contentful.com>
2025-11-18 12:50:30 -0700

    update to vitest v4

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    dir: './test',
+    coverage: {
+      include: ['src/**.{js,jsx,ts,tsx}'],
+    },
   },
 })
